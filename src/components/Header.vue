d0db2f31ba7d704d44d7f8ea67cd2a2d5da591da
unknown <m.ezz.ibr@gmail.com>
2021-09-19 18:26:34 +0200

    mohamed -m sss

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+<header>
+
+  <h1>
+    {{ title }}
+  </h1>
+</header>
+
+</template>
+
+<script>
+export default {
+ props: ['title']
+}
+
+</script>
+
+<style scoped>
+header {
+  padding: 24px 32px;
+  display: flex;
+  justify-content: center;
+}
+h1 {
+  font-size: 32px;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="quote">
+  <div class="quote-content">
+    {{ quote.content }}
+<div class="quote-info">
+  <div class="quote-anime">
+    {{ quote.anime }}
+  </div>
+  <div class="quote-author">
+    {{ quote.char }}
+
+  </div>
+
+</div>
+
+  </div>
+
+    {{ quote.test }}
+  </section>
+
+</template>
+
+<script>
+export default {
+  props: ['quote']
+}
+
+</script>
+
+<style scoped>
+.quote {
+  position: relative;
+  margin: 0 auto;
+  padding: 32px;
+  max-width: 720px;
+
+
+}
+.quote-content {
+  position: relative;
+  font-size: 28px;
+  font-weight: 600;
+  padding: 48px 32px;
+  border-radius: 16px ;
+  background-color: darkslategray;
+  color: #84afff;
+}
+.quote-info {
+  position: relative;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding: 32px;
+  font-weight: 300;
+}
+
+</style>
